da20a09d84f564058cfc9fd9cd83d708c39c29ae
Saleh Khatri <salehkhatri29@gmail.com>
2025-03-24 14:09:21 +0530

    Updated description

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -2,9 +2,6 @@ import { generateSEO } from "@/lib/seo";
 
 export const metadata = generateSEO({
   title: "About Aakaura",
-  description:
-    "Aakaura is a sanctuary for spiritual seekers, offering handcrafted decor, healing crystals, chakra balancing, meditation guides, and holistic well-being resources.",
-  image: "/images/splashLog.png",
   url: "https://aakaura.in/about",
 });
 export default function AboutLayout({
